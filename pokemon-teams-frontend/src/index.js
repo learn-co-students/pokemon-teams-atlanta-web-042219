4cbbd7e436b1d656d3da3b13ffbfa0c94bcd5bd1
ascotttoney <ascotttoney@gmail.com>
2019-06-07 15:10:25 -0400

    Done.

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,41 @@
-const BASE_URL = "http://localhost:3000"
-const TRAINERS_URL = `${BASE_URL}/trainers`
-const POKEMONS_URL = `${BASE_URL}/pokemons`
+document.addEventListener("DOMContentLoaded", () => {
+  const BASE_URL = `http://localhost:3000`
+  const TRAINERS_URL = `${BASE_URL}/trainers`
+  const POKEMONS_URL = `${BASE_URL}/pokemons`
+  const main = document.querySelector('main')
+
+  fetch(TRAINERS_URL)
+    .then(res => res.json())
+    // .then(console.log)
+    .then(data => {
+      data.forEach(addTrainer)
+      // data.forEach(console.log("DATA", data[0]['pokemons'][0].trainer_id))
+    })
+
+// -------------------------------------------------------------- //
+
+  function addTrainer(trainer){
+    let pokemonLi = ''
+    trainer.pokemons.forEach(pokemon => {
+      pokemonLi += `<li> ${pokemon.nickname} (${pokemon.species}) <button class="release" data-pokemon-id=${pokemon.id}>Release</button></li>`
+    })
+
+    main.innerHTML +=
+    `<div class="card" data-id=${trainer.id}><p>${trainer.name}</p>
+      <button data-trainer-id=${trainer.id}>Add Pokemon</button>
+      <ul>
+        ${pokemonLi}
+      </ul>
+    </div>`
+  }
+
+// -------------------------------------------------------------- //
+
+  function addPokemon() {
+    
+  }  
+
+// -------------------------------------------------------------- //
+// FINAL BRACKETS BELOW
+
+})
