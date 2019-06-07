c7660a2c66a4ff14ca9dec8a4fbd961258b1806b
ascotttoney <ascotttoney@gmail.com>
2019-06-07 19:08:50 -0400

    Done.

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,20 +1,27 @@
+
 document.addEventListener("DOMContentLoaded", () => {
   const BASE_URL = `http://localhost:3000`
   const TRAINERS_URL = `${BASE_URL}/trainers`
   const POKEMONS_URL = `${BASE_URL}/pokemons`
   const main = document.querySelector('main')
 
-  fetch(TRAINERS_URL)
-    .then(res => res.json())
-    // .then(console.log)
-    .then(data => {
-      data.forEach(addTrainer)
-      // data.forEach(console.log("DATA", data[0]['pokemons'][0].trainer_id))
-    })
+  document.addEventListener('click', handleEvents)
+
+  getTrainers();
 
 // -------------------------------------------------------------- //
 
-  function addTrainer(trainer){
+  function getTrainers() {
+    fetch(TRAINERS_URL)
+    .then(res => res.json())
+    .then(data => {
+      data.forEach(showTrainers)
+    })
+  }
+
+// -------------------------------------------------------------- //
+
+  function showTrainers(trainer){
     let pokemonLi = ''
     trainer.pokemons.forEach(pokemon => {
       pokemonLi += `<li> ${pokemon.nickname} (${pokemon.species}) <button class="release" data-pokemon-id=${pokemon.id}>Release</button></li>`
@@ -23,19 +30,43 @@ document.addEventListener("DOMContentLoaded", () => {
     main.innerHTML +=
     `<div class="card" data-id=${trainer.id}><p>${trainer.name}</p>
       <button data-trainer-id=${trainer.id}>Add Pokemon</button>
-      <ul>
-        ${pokemonLi}
-      </ul>
+      <ul> ${pokemonLi}</ul>
     </div>`
   }
 
 // -------------------------------------------------------------- //
 
-  function addPokemon() {
-    
-  }  
+  function handleEvents(e) {
+    const trainerID = e.target.parentElement.dataset.id
+    const pokemon = e.target.parentElement
 
-// -------------------------------------------------------------- //
-// FINAL BRACKETS BELOW
+    if (e.target.innerText === 'Add Pokemon') {
+      fetch(POKEMONS_URL, {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+          Accepts: 'application/json'
+        },
+        body: JSON.stringify({
+          trainer_id: trainerID
+        })
+      })
+      .then(res => res.json())
+      .then(res => {
+        newPokemon = e.target.nextSibling.nextSibling
+        newPokemon.innerHTML +=
+          `<li> ${res.nickname} (${res.species}) <button class="release" data-pokemon-id=${res.id}>Release</button></li>`
+      })
+    }
+    
+    else if (e.target.innerText === 'Release') {
+      fetch(`${POKEMONS_URL}/${e.target.dataset.pokemonId}`, {
+        method: 'DELETE'
+      })
+      pokemon.remove()
+    }
+  }
+
+// FINAL BRACKETS BELOW ----------------------------------------- //
 
 })
